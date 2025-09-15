6185c46765f8c95b9c26fc6547d78f9b2793469d
kaustubh <kaustubh.mp007@gmail.com>
2025-09-15 18:31:55 +0530

    Added Vercel URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Middlewar
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://course-hive-fe.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
